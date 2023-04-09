8f4dba05ac1d4b0a2c10e027235be3779f8a32de
Olena Husliakova <113473257+softly242@users.noreply.github.com>
2023-04-09 23:29:53 +0200

    .

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -36,18 +36,3 @@ function onSubmit(event) {
       });
   }
 }
-
-/* createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  }); */
